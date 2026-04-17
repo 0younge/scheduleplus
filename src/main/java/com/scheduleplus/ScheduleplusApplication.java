5bba091d7a14d9122dc10bf8daf1b04fa90ae7b0
0younge <s45628439@gmail.com>
2026-04-17 09:28:58 +0900

    first commit

---
+++ src/main/java/com/scheduleplus/ScheduleplusApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduleplus/ScheduleplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

16a65b3f43eafa18d697e2a62a0a9f920ec79ca8
0younge <s45628439@gmail.com>
2026-04-18 23:41:48 +0900

    feat: globalException 처리
    - 전체 예외 처리

---
+++ src/main/java/com/scheduleplus/common/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.scheduleplus.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+    public GlobalExceptionHandler(String message) {
+        super(message);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("요청 오류: " + e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}

b2229f883e9fc389f125ed015dde7d3427a0e479
0younge <s45628439@gmail.com>
2026-04-19 15:42:28 +0900

    refactor: User 컨트롤러 리팩토링
    누락되었던 컨트롤러 리팩토링을 진행하였습니다
    - 긴 코드를 짧게 수정
    
    - 주석 추가

--- src/main/java/com/scheduleplus/user/controller/UserController.java
+++ src/main/java/com/scheduleplus/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.scheduleplus.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,28 +17,50 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * 유저 전체 조회
+     * @return 유저 이름, 생성/수정일 반환
+     */
     @GetMapping
     public ResponseEntity<List<GetUserResponse>> getAllUser() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getAll());
+        return ResponseEntity.ok(userService.getAll());
     }
 
+    /**
+     * 특정 유저 조회
+     * @param userId 조회하고자 하는 유저 아이디
+     * @return 유저 이름, 이메일, 생성/수정일 반환
+     */
     @GetMapping("/{userId}")
     public ResponseEntity<GetOneUserResponse> getOneUser(@PathVariable Long userId) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getOne(userId));
+        return ResponseEntity.ok(userService.getOne(userId));
     }
 
+    /**
+     * 유저 수정
+     * @param userId 수정하고자 하는 유저 아이디
+     * @param session 검증을 위한 세션
+     * @param request 수정할 유저이름, 이메일
+     * @return 상태코드
+     */
     @PatchMapping("/{userId}")
     public ResponseEntity<Void> updateUser(@PathVariable Long userId,
                                            HttpSession session,
                                            @RequestBody @Valid UpdateUserRequest request) {
         userService.update(userId, session, request);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
+    /**
+     * 유저 삭제
+     * @param userId 삭제하고자 하는 유저 아이디
+     * @param session 검증을 위한 세션
+     * @return 상태코드
+     */
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId, HttpSession session) {
         userService.delete(userId, session);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.noContent().build();
     }
 
 }

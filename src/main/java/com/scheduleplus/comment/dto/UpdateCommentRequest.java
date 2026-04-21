ef7bbd1479433c20b93620cea98b92d27369155b
0younge <s45628439@gmail.com>
2026-04-22 00:54:47 +0900

    refactor: Comment Dto
    - 댓글 내용에 필요한 제한값을 추가하였습니다

--- src/main/java/com/scheduleplus/comment/dto/UpdateCommentRequest.java
+++ src/main/java/com/scheduleplus/comment/dto/UpdateCommentRequest.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public class UpdateCommentRequest {
 
     @NotBlank(message = "내용은 공백을 허용하지 않습니다.")
-    @Size(max = 100)
+    @Size(max = 100, message = "내용은 100자 이내로 작성해야 합니다.")
     private final String content;
 
     public UpdateCommentRequest(String content) {
